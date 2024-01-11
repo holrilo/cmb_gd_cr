889e0beecd1b370a8ab9e4c8ebfd239881f3eade
holrilo <senky98@gmail.com>
2024-01-11 15:55:14 -0500

    Se crea sidebar con material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,28 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/auth/login/login.component";
 import { NgModel } from '@angular/forms';
 
+// import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,LoginComponent],
+  imports: [CommonModule, RouterOutlet,LoginComponent, 
+    // MatToolbarModule,
+    // MatSidenavModule,
+    // FormsModule,
+    // ReactiveFormsModule,
+    // MatCheckboxModule,
+    // MatFormFieldModule,
+    // MatButtonModule,
+    // MatInputModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,4 +34,14 @@ import { NgModel } from '@angular/forms';
 
 export class AppComponent {
   title = 'ControlRecepcion';
+
+  options = this._formBuilder.group({
+    bottom: 0,
+    // fixed: false,
+    top: 0,
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(window.location.host);
 }
--- src/app/components/pages/container-app/container-app.component.ts
+++ src/app/components/pages/container-app/container-app.component.ts
@@ -1,14 +1,74 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 import { RouterOutlet } from '@angular/router';
+import { AppComponent } from '../../../app.component';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import { CommonModule} from '@angular/common';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon'
+import {MatListModule} from '@angular/material/list'
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-container-app',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, AppComponent,
+    MatToolbarModule,
+    MatSidenavModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    CommonModule
+    ],
   templateUrl: './container-app.component.html',
   styleUrl: './container-app.component.scss'
 })
 export class ContainerAppComponent {
+//   options = this._formBuilder.group({
+//     bottom: 0,
+//     // fixed: false,
+//     top: 0,
+//   });
+
+//   constructor(private _formBuilder: FormBuilder) {}
+
+//   shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(window.location.host);
+
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  isMobile= true;
+  isCollapsed = false;
+
+  constructor(private observe: BreakpointObserver){}
+
+  ngOnInit(){
+    this.observe.observe(['(max-width: 800px']).subscribe((ScreenSize)=>{
+      if(ScreenSize.matches){
+        this.isMobile = true;
+      }else{
+        this.isMobile = false;
+      }
+    });
+  }
+  toggleMenu(){
+    if (this.isMobile) {
+      this.sidenav.toggle();
+      this.isCollapsed = false;
+    }else{
+      this.sidenav.open();
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
 
 }
